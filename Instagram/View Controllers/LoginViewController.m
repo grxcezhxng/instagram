4af2db7ce94bbf76a264a2d3b689264f9da8f157
Grace Zhang <grxcezhxng@gmail.com>
2021-07-09 17:53:04 -0700

    more ui changes

--- Instagram/View Controllers/LoginViewController.m
+++ Instagram/View Controllers/LoginViewController.m
@@ -22,6 +22,14 @@
     [super viewDidLoad];
     self.usernameField.delegate = self;
     self.passwordField.delegate = self;
+    
+    UIColor const *leftColor = [UIColor colorWithRed:178.0/255.0 green:30.0/255.0 blue:175.0/255.0 alpha:1.0];
+    UIColor const *middleColor = [UIColor colorWithRed:193.0/255.0 green:42.0/255.0 blue:111.0/255.0 alpha:1.0];
+    UIColor const *rightColor = [UIColor colorWithRed:210.0/255.0 green:61.0/255.0 blue:95.0/255.0 alpha:1.0];
+    CAGradientLayer *theViewGradient = [CAGradientLayer layer];
+    theViewGradient.colors = [NSArray arrayWithObjects: (id)leftColor.CGColor, (id)middleColor.CGColor,(id)rightColor.CGColor, nil];
+    theViewGradient.frame = self.view.bounds;
+    [self.view.layer insertSublayer:theViewGradient atIndex:0];
 }
 
 - (IBAction)handleSignup:(id)sender {
